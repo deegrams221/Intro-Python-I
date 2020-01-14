fd0314756ce985ca372bbcbbf32cbff3cbfddd73
Diana Grams <deegrams221@gmail.com>
2020-01-14 15:07:34 -0700

    re-wrote the answer for 'Modify the dictionary' to a simpler form

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -45,10 +45,14 @@ print('Waypoints:', waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
-for way in waypoints:
-    way.update((key, -130) for key, value in way.items() if value == -121)
-    way.update((key, "not a real place") for key, value in way.items() if value == 'a place')
-    print(way)
+# for way in waypoints:
+#     way.update((key, -130) for key, value in way.items() if value == -121)
+#     way.update((key, "not a real place") for key, value in way.items() if value == 'a place')
+#     print(way)
+
+waypoints[0]["name"] ="not a real place"
+waypoints[0]["lon"] = -130
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
